72f3bac9b77b7b2813c27626ea77b1f62c5ba190
NOVA\sakthsa <santhosh.sakthivel@tecnotree.com>
2019-02-05 16:57:04 +0530

    lastest code

--- Java Workspace/mavenn/src/test/java/com/ss/test/RemoveAllSpclChar.java
+++ Java Workspace/mavenn/src/test/java/com/ss/test/RemoveAllSpclChar.java
@@ -5,7 +5,5 @@ public class RemoveAllSpclChar {
 	public static void main(String[] args) {
 		String str = "@##*$*@SanTHoSH29339!$#)%@$%(%&";
 		System.out.println(str.replaceAll("[^a-zA-Z0-9]", ""));
-
 	}
-
 }
